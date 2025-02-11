a25566851f48ea5986a99c01e08080a15d198e6f
Jean C. Lima <31942877+ojeanclima@users.noreply.github.com>
2025-02-11 16:29:01 -0300

    Create filter-pagina-de-captura-tag-de-identificacao.php

---
+++ php/filter-pagina-de-captura-tag-de-identificacao.php
@@ -0,0 +1,23 @@
+<?php
+
+// Adicionar filtro de tags na tabela de listagem do Custom Post Type
+function add_tag_filter_to_admin_list() {
+    global $typenow;
+    $post_type = 'pagina-de-captura'; // Nome do Custom Post Type
+    $taxonomy = 'tag-de-identificacao'; // Nome da taxonomia
+
+    if ($typenow == $post_type) {
+        wp_dropdown_categories(array(
+            'show_option_all' => 'Todas as Tags',
+            'taxonomy' => $taxonomy,
+            'name' => $taxonomy,
+            'orderby' => 'name',
+            'selected' => isset($_GET[$taxonomy]) ? $_GET[$taxonomy] : '',
+            'show_count' => true,
+            'hide_empty' => true,
+            'value_field' => 'slug'
+        ));
+    }
+}
+
+add_action('restrict_manage_posts', 'add_tag_filter_to_admin_list');
